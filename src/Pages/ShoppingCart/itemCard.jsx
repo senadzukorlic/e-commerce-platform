fd1cd23319d68485d5ef97e539b1a3f2b24e2659
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-09-21 07:53:19 +0200

    created favorite button component for switch hearts and reverted to the old functionality

--- src/Pages/ShoppingCart/itemCard.jsx
+++ src/Pages/ShoppingCart/itemCard.jsx
@@ -2,10 +2,11 @@ import { useState, useEffect } from "react"
 import { useDataContext } from "../../Hooks/useContext"
 import Box from "@mui/material/Box"
 import DeleteIcon from "@mui/icons-material/Delete"
-import FavoriteIcon from "@mui/icons-material/Favorite"
+import FilledHeartIcon from "../../Styles/filledHeartIcon"
+import OutlinedHeartIcon from "../../Styles/outlinedHeartIcon"
+import FavoriteButton from "../../Components/FavoriteButton/FavoriteButton"
 
 import {
-  HeartIcon,
   StyledItemCard,
   CardContentImageStyled,
   Image,
@@ -17,7 +18,6 @@ import {
   ItemInfoDiv,
   PDiv,
   IconDiv,
-  HeartButton,
   QuantityDiv,
   Select,
   MenuItemStyled,
@@ -139,27 +139,13 @@ export function ItemCard() {
               </PDiv>
             </ItemInfoDiv>
             <IconDiv>
-              <HeartButton onClick={() => setFavorite(item)}>
-                {!favoriteButton.includes(item.id) ? (
-                  <HeartIcon
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fas"
-                    data-icon="heart"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                    style={{ color: "red" }}
-                  >
-                    <path
-                      fill="#ff0000"
-                      d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"
-                    ></path>
-                  </HeartIcon>
-                ) : (
-                  <FavoriteIcon style={{ color: "red" }} />
-                )}
-              </HeartButton>
+              <FavoriteButton
+                item={item}
+                favoriteItems={favoriteItems}
+                setFavoriteItems={setFavoriteItems}
+                FilledIcon={FilledHeartIcon}
+                OutlinedIcon={OutlinedHeartIcon}
+              />
               <QuantityDiv>
                 <Select
                   value={quantities[item.id]}

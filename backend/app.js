eb2c9d8f085bb07cd46bca60f9b7338a7ac16394
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-09 23:33:28 +0200

    set headers for dissable CORE protection and some another things

--- backend/app.js
+++ backend/app.js
@@ -3,3 +3,15 @@ const express = require("express")
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Acces-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  )
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization")
+  next()
+})
+
+app.listen(8080, console.log("Server running on port 8080"))

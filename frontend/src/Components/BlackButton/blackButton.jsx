e1978d53505559b89039386ce80537980d574ef4
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-11-02 14:05:53 +0100

    redesinged singup page

--- frontend/src/Components/BlackButton/blackButton.jsx
+++ frontend/src/Components/BlackButton/blackButton.jsx
@@ -1,9 +1,21 @@
 import { StyleButton } from "./styleBlackButton"
 
-export function BlackButton({ width, buttonName, type, onClick }) {
+export function BlackButton({
+  width,
+  buttonName,
+  type,
+  onClick,
+
+  onSubmit,
+}) {
   return (
     <>
-      <StyleButton style={width} type={type} onClick={onClick}>
+      <StyleButton
+        style={width}
+        type={type}
+        onClick={onClick}
+        onSubmit={onSubmit}
+      >
         {buttonName}
       </StyleButton>
     </>
--- frontend/src/Pages/Register/register.jsx
+++ frontend/src/Pages/Register/register.jsx
@@ -1,85 +1,8 @@
-// import { useState } from "react"
-// import { Input } from "../../Components/Input/input"
-// import { PasswordInput } from "../../Components/PasswordInput/passwordInput"
-// import { BlackButton } from "../../Components/BlackButton/blackButton"
-// import {
-//   RegisterContainer,
-//   RegisterTitle,
-//   CheckboxContainer,
-//   Checkbox,
-//   TermsText,
-//   TermsLink,
-// } from "./styleRegister"
-
-// export function Register() {
-//   const [password, setPassword] = useState("")
-//   const [showPassword, setShowPassword] = useState(false)
-//   const [date, setDate] = useState("")
-
-//   const handlePasswordChange = (event) => {
-//     setPassword(event.target.value)
-//   }
-
-//   const togglePasswordVisibility = () => {
-//     setShowPassword(!showPassword)
-//   }
-
-//   return (
-//     <RegisterContainer>
-//       <RegisterTitle>Become a JacpiStore member</RegisterTitle>
-
-//       <Input
-//         type="text"
-//         labelName="Email"
-//         styleInput={{ width: "40%" }}
-//         styleLabel={{ width: "41%" }}
-//       />
-
-//       <PasswordInput
-//         type={showPassword ? "text" : "password"}
-//         label="Create a password"
-//         value={password}
-//         right="400px"
-//         onChange={handlePasswordChange}
-//         setVisibility={togglePasswordVisibility}
-//         booleanVisibility={showPassword ? "Hide" : "Show"}
-//         styleLabel={{ width: "41%", marginBottom: "0" }}
-//         styleInput={{ width: "40%", marginBottom: "0" }}
-//       />
-//       <PasswordInput
-//         type={showPassword ? "text" : "password"}
-//         label="Confrim password"
-//         value={password}
-//         right="400px"
-//         onChange={handlePasswordChange}
-//         setVisibility={togglePasswordVisibility}
-//         booleanVisibility={showPassword ? "Hide" : "Show"}
-//         styleLabel={{ width: "41%", marginBottom: "0" }}
-//         styleInput={{ width: "40%", marginBottom: "0" }}
-//       />
-
-//       <CheckboxContainer>
-//         <Checkbox type="checkbox" />
-//         <p>
-//           Yes, email me my member rewards, special invites, trend alerts, and
-//           more.
-//         </p>
-//       </CheckboxContainer>
-
-//       <TermsText>
-//         By clicking ‘Become a member’, I agree to the H&M Membership
-//         <TermsLink href="/dsafsa">Terms and conditions.</TermsLink>
-//       </TermsText>
-
-//       <BlackButton buttonName="Become a  member" width={{ width: "40%" }} />
-//     </RegisterContainer>
-//   )
-// }
 import { useState } from "react"
-import axios from "axios"
 import { Input } from "../../Components/Input/input"
 import { PasswordInput } from "../../Components/PasswordInput/passwordInput"
 import { BlackButton } from "../../Components/BlackButton/blackButton"
+import axios from "axios"
 import {
   RegisterContainer,
   RegisterTitle,
@@ -90,20 +13,18 @@ import {
 } from "./styleRegister"
 
 export function Register() {
-  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
+  const [email, setEmail] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
   const [error, setError] = useState(null)
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value)
   }
-
   const handlePasswordChange = (event) => {
     setPassword(event.target.value)
   }
-
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value)
   }
@@ -127,60 +48,65 @@ export function Register() {
       console.log(response.data)
       // Handle successful registration (e.g., redirect to login page)
     } catch (err) {
-      setError(err.response.data.message)
+      console.log("Ne radi", err)
     }
   }
 
   return (
     <RegisterContainer>
       <RegisterTitle>Become a JacpiStore member</RegisterTitle>
-      <form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          labelName="Email"
-          value={email}
-          onChange={handleEmailChange}
-          styleInput={{ width: "40%" }}
-          styleLabel={{ width: "41%" }}
-        />
-        <PasswordInput
-          type={showPassword ? "text" : "password"}
-          label="Create a password"
-          value={password}
-          onChange={handlePasswordChange}
-          setVisibility={togglePasswordVisibility}
-          booleanVisibility={showPassword ? "Hide" : "Show"}
-          styleLabel={{ width: "41%", marginBottom: "0" }}
-          styleInput={{ width: "40%", marginBottom: "0" }}
-        />
-        <PasswordInput
-          type={showPassword ? "text" : "password"}
-          label="Confirm password"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-          setVisibility={togglePasswordVisibility}
-          booleanVisibility={showPassword ? "Hide" : "Show"}
-          styleLabel={{ width: "41%", marginBottom: "0" }}
-          styleInput={{ width: "40%", marginBottom: "0" }}
-        />
-        {error && <p>{error}</p>}
-        <CheckboxContainer>
-          <Checkbox type="checkbox" />
-          <p>
-            Yes, email me my member rewards, special invites, trend alerts, and
-            more.
-          </p>
-        </CheckboxContainer>
-        <TermsText>
-          By clicking ‘Become a member’, I agree to the H&M Membership
-          <TermsLink href="/dsafsa">Terms and conditions.</TermsLink>
-        </TermsText>
-        <BlackButton
-          buttonName="Become a member"
-          width={{ width: "40%" }}
-          onClick={handleSubmit}
-        />
-      </form>
+
+      <Input
+        type="text"
+        labelName="Email"
+        styleInput={{ width: "40%" }}
+        styleLabel={{ width: "41%" }}
+        onChange={handleEmailChange}
+        value={email}
+      />
+
+      <PasswordInput
+        type={showPassword ? "text" : "password"}
+        label="Create a password"
+        value={password}
+        right="400px"
+        onChange={handlePasswordChange}
+        setVisibility={togglePasswordVisibility}
+        booleanVisibility={showPassword ? "Hide" : "Show"}
+        styleLabel={{ width: "41%", marginBottom: "0" }}
+        styleInput={{ width: "40%", marginBottom: "0" }}
+      />
+      <PasswordInput
+        type={showPassword ? "text" : "password"}
+        label="Confrim password"
+        value={confirmPassword}
+        right="400px"
+        onChange={handleConfirmPasswordChange}
+        setVisibility={togglePasswordVisibility}
+        booleanVisibility={showPassword ? "Hide" : "Show"}
+        styleLabel={{ width: "41%", marginBottom: "0" }}
+        styleInput={{ width: "40%", marginBottom: "0" }}
+      />
+
+      <CheckboxContainer>
+        <Checkbox type="checkbox" />
+        <p>
+          Yes, email me my member rewards, special invites, trend alerts, and
+          more.
+        </p>
+      </CheckboxContainer>
+
+      <TermsText>
+        By clicking ‘Become a member’, I agree to the JacpiStore Membership
+        <TermsLink href="/dsafsa">Terms and conditions.</TermsLink>
+      </TermsText>
+
+      <BlackButton
+        type={"submit"}
+        onClick={handleSubmit}
+        buttonName="Become a  member"
+        width={{ width: "40%" }}
+      />
     </RegisterContainer>
   )
 }

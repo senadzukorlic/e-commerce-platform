e56318a99f26b199366a6e91f90e4dd8209a6f31
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-12-01 10:23:32 +0100

    created functionality for adding own products on cart

--- frontend/src/Pages/ShoppingCart/itemCard.jsx
+++ frontend/src/Pages/ShoppingCart/itemCard.jsx
@@ -1,174 +1,3 @@
-// import { useState, useEffect } from "react"
-// import { useDataContext } from "../../Hooks/useContext"
-// import Box from "@mui/material/Box"
-// import DeleteIcon from "@mui/icons-material/Delete"
-// import FilledHeartIcon from "../../Styles/filledHeartIcon"
-// import OutlinedHeartIcon from "../../Styles/outlinedHeartIcon"
-// import FavoriteButton from "../../Components/FavoriteButton/FavoriteButton"
-
-// import {
-//   StyledItemCard,
-//   CardContentImageStyled,
-//   Image,
-//   CardContentTitleStyled,
-//   TitleAndIconDiv,
-//   TitleDiv,
-//   IconDiv1,
-//   DeleteButton,
-//   ItemInfoDiv,
-//   PDiv,
-//   IconDiv,
-//   QuantityDiv,
-//   Select,
-//   MenuItemStyled,
-// } from "./itemCardStyle"
-
-// export function ItemCard() {
-//   const {
-//     cartData,
-//     setCartData,
-//     setTotal,
-//     setCartCount,
-//     favoriteItems,
-//     setFavoriteItems,
-//   } = useDataContext()
-//   const [favoriteButton, setFavoriteButton] = useState([])
-
-//   useEffect(() => {
-//     const initialFavoriteButtons = cartData
-//       .filter((item) => favoriteItems.some((favItem) => favItem.id === item.id))
-//       .map((item) => item.id)
-
-//     setFavoriteButton(initialFavoriteButtons)
-//   }, [cartData, favoriteItems])
-
-//   const handleQuantityChange = (itemId) => (event) => {
-//     const newQuantity = event.target.value
-//     setQuantities((prevQuantities) => {
-//       const updatedQuantities = {
-//         ...prevQuantities,
-//         [itemId]: newQuantity,
-//       }
-//       const newTotal = cartData.reduce((acc, item) => {
-//         return acc + item.price * updatedQuantities[item.id]
-//       }, 0)
-//       setTotal(newTotal)
-
-//       return updatedQuantities
-//     })
-//   }
-
-//   const [quantities, setQuantities] = useState(
-//     cartData.reduce((acc, item) => {
-//       acc[item.id] = 1
-//       return acc
-//     }, {})
-//   )
-
-//   const deleteItem = (item) => {
-//     setCartData((currentItem) => {
-//       const updatedItems = currentItem.filter(
-//         (cartItem) => cartItem.id !== item.id
-//       )
-//       const total = updatedItems.reduce((accumulator, cartItem) => {
-//         return accumulator + cartItem.price
-//       }, 0)
-//       setTotal(total)
-//       setCartCount((currentValue) => currentValue - 1)
-//       return updatedItems
-//     })
-//   }
-
-//   const setFavorite = (item) => {
-//     setFavoriteButton((oldItems) => {
-//       if (oldItems.includes(item.id)) {
-//         setFavoriteItems((currentItems) =>
-//           currentItems.filter((favItem) => favItem.id !== item.id)
-//         )
-//         return oldItems.filter((id) => id !== item.id)
-//       } else {
-//         setFavoriteItems((currentItems) => {
-//           if (!currentItems.some((favItem) => favItem.id === item.id)) {
-//             return [...currentItems, item]
-//           }
-//           return currentItems
-//         })
-//         return [...oldItems, item.id]
-//       }
-//     })
-//   }
-
-//   return (
-//     <>
-//       {cartData.map((item) => (
-//         <StyledItemCard key={item.id} variant="outlined">
-//           <CardContentImageStyled>
-//             <Box>
-//               <Image component="img" image={item.images[0]} />
-//             </Box>
-//           </CardContentImageStyled>
-//           <CardContentTitleStyled>
-//             <TitleAndIconDiv>
-//               <TitleDiv gutterBottom variant="p">
-//                 {item.title}
-//                 <br />
-//                 {item.price}€
-//               </TitleDiv>
-//               <IconDiv1>
-//                 <DeleteButton onClick={() => deleteItem(item)}>
-//                   <DeleteIcon />
-//                 </DeleteButton>
-//               </IconDiv1>
-//             </TitleAndIconDiv>
-//             <ItemInfoDiv>
-//               <PDiv>
-//                 <p>Art.No.</p>
-//                 <p>Colour:</p>
-//               </PDiv>
-//               <PDiv>
-//                 <p style={{ fontWeight: "bolder" }}>{item.sku}</p>
-//                 <p style={{ fontWeight: "bolder" }}>White</p>
-//               </PDiv>
-//               <PDiv>
-//                 <p>Size:</p>
-//                 <p>Total:</p>
-//               </PDiv>
-//               <PDiv>
-//                 <p style={{ fontWeight: "bolder" }}>{item.size}</p>
-//                 <p style={{ fontWeight: "bolder" }}>{item.price}</p>
-//               </PDiv>
-//             </ItemInfoDiv>
-//             <IconDiv>
-// <FavoriteButton
-//   item={item}
-//   favoriteItems={favoriteItems}
-//   setFavoriteItems={setFavoriteItems}
-//   FilledIcon={FilledHeartIcon}
-//   OutlinedIcon={OutlinedHeartIcon}
-// />
-//               <QuantityDiv>
-//                 <Select
-//                   value={quantities[item.id]}
-//                   onChange={handleQuantityChange(item.id)}
-//                 >
-//                   {[...Array(10).keys()].map((number) => (
-//                     <MenuItemStyled
-//                       key={`quantity-${number + 1}`}
-//                       value={number + 1}
-//                     >
-//                       {number + 1}
-//                     </MenuItemStyled>
-//                   ))}
-//                 </Select>
-//               </QuantityDiv>
-//             </IconDiv>
-//           </CardContentTitleStyled>
-//         </StyledItemCard>
-//       ))}
-//     </>
-//   )
-// }
-
 import { useState, useEffect } from "react"
 import { useDataContext } from "../../Hooks/useContext"
 import Box from "@mui/material/Box"
@@ -176,6 +5,7 @@ import DeleteIcon from "@mui/icons-material/Delete"
 import FilledHeartIcon from "../../Styles/filledHeartIcon"
 import OutlinedHeartIcon from "../../Styles/outlinedHeartIcon"
 import FavoriteButton from "../../Components/favoriteButton/favoriteButton"
+import axios from "axios"
 
 import {
   StyledItemCard,
@@ -204,6 +34,7 @@ export function ItemCard() {
     setFavoriteItems,
   } = useDataContext()
   const [favoriteButton, setFavoriteButton] = useState([])
+  const [ownProducts, setOwnProducts] = useState([])
 
   useEffect(() => {
     const initialFavoriteButtons = cartData
@@ -280,10 +111,28 @@ export function ItemCard() {
     })
   }
 
+  const handleFetchOwnProducts = async () => {
+    try {
+      const token = localStorage.getItem("token")
+      const resposne = await axios.get(
+        "http://localhost:8080/admin/my-products",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      setOwnProducts(resposne.data.products)
+    } catch (error) {
+      console.log("Nesto nije dobro", error)
+    }
+  }
+
+  useEffect(() => {
+    handleFetchOwnProducts()
+  }, [])
   return (
     <>
       {cartData.map((item) => (
-        <StyledItemCard key={`${item.id}-${item.size}`} variant="outlined">
+        <StyledItemCard key={item.id} variant="outlined">
           <CardContentImageStyled>
             <Box>
               <Image component="img" image={item.images[0]} />
@@ -343,6 +192,72 @@ export function ItemCard() {
                   ))}
                 </Select>
               </QuantityDiv>
+            </IconDiv>
+          </CardContentTitleStyled>
+        </StyledItemCard>
+      ))}
+
+      {ownProducts.map((item) => (
+        <StyledItemCard key={item.id} variant="outlined">
+          <CardContentImageStyled>
+            <Box>
+              <Image component="img" image={item.imageUrl} />
+            </Box>
+          </CardContentImageStyled>
+          <CardContentTitleStyled>
+            <TitleAndIconDiv>
+              <TitleDiv gutterBottom variant="p">
+                {item.title}
+                <br />
+                {item.price}€
+              </TitleDiv>
+              <IconDiv1>
+                <DeleteButton onClick={() => deleteItem(item)}>
+                  <DeleteIcon />
+                </DeleteButton>
+              </IconDiv1>
+            </TitleAndIconDiv>
+            <ItemInfoDiv>
+              <PDiv>
+                <p>Art.No.</p>
+                <p>Colour:</p>
+              </PDiv>
+              <PDiv>
+                <p style={{ fontWeight: "bolder" }}>6RJDTVCU</p>
+                <p style={{ fontWeight: "bolder" }}>White</p>
+              </PDiv>
+              <PDiv>
+                <p>Size:</p>
+                <p>Total:</p>
+              </PDiv>
+              <PDiv>
+                <p style={{ fontWeight: "bolder" }}>{item.size}</p>
+                <p style={{ fontWeight: "bolder" }}>{item.price}</p>
+              </PDiv>
+            </ItemInfoDiv>
+            <IconDiv>
+              <FavoriteButton
+                item={item}
+                favoriteItems={favoriteItems}
+                setFavoriteItems={setFavoriteItems}
+                FilledIcon={FilledHeartIcon}
+                OutlinedIcon={OutlinedHeartIcon}
+              />
+              {/* <QuantityDiv>
+                <Select
+                  value={quantities[`${item.id}-${item.size}`]}
+                  onChange={handleQuantityChange(item.id, item.size)}
+                >
+                  {[...Array(10).keys()].map((number) => (
+                    <MenuItemStyled
+                      key={`quantity-${number + 1}`}
+                      value={number + 1}
+                    >
+                      {number + 1}
+                    </MenuItemStyled>
+                  ))}
+                </Select>
+              </QuantityDiv> */}
             </IconDiv>
           </CardContentTitleStyled>
         </StyledItemCard>
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -9,5 +9,6 @@ route.post("/my-products", isAuth, adminRoutes.createProduct)
 route.delete("/my-products/:productId", isAuth, adminRoutes.deleteProduct)
 route.patch("/my-products/:productId", isAuth, adminRoutes.editProduct)
 route.post("/my-products/:productId", isAuth, adminRoutes.addToCart)
+route.get("/my-products", isAuth, adminRoutes.getCart)
 
 module.exports = route
